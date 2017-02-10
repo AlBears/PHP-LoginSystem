2007ca02c342056713c54d1d8454cef4151e6785
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-02-10 11:34:10 +0300

    add mail support

--- classes/Config.class.php
+++ classes/Config.class.php
@@ -10,6 +10,10 @@ class Config
   const DB_HOST = '127.0.0.1',
         DB_NAME = 'php_login_register',
         DB_USER = 'root',
-        DB_PASS = 'root';
+        DB_PASS = 'root',
+
+        SMTP_HOST = 'smtp.mailtrap.io',
+        SMTP_USER = '4873a1962ce2ea',
+        SMTP_PASS = '16fb7be7d528c7';
 
 }
